4dc283312fab64dc72413982a1c23c2d19f9c5fb
Sergio Sainz <100535668+sergiolplp@users.noreply.github.com>
2026-05-02 21:24:13 -0300

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = "sorteos-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        "./",
+        "./index.html",
+        "./quinelasolicaria.mp3"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
